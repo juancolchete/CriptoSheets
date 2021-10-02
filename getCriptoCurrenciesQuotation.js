fe04b956739c5196e7748bdb1e6f52c343926cae
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-10-02 01:02:49 -0300

    Update getCriptoCurrenciesQuotation.js

--- getCriptoCurrenciesQuotation.js
+++ getCriptoCurrenciesQuotation.js
@@ -1,20 +1,27 @@
 function getCriptoCurrenciesQuotation() {
-  var tokenId = "1,1027,1839,2010,3992,825,4687,7186,9377,11130,1958,11387"//variable with the criptocurrencies ids
-
+  tokensArray = getTokens();
   var url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?'
-    + 'id='+tokenId
+    + 'id='+tokensArray
   var options = {
      "method" : "get",
      "headers" : {
-       "X-CMC_PRO_API_KEY" : "YOUR_API_KEY"//API key from coinmaketcap
+       "X-CMC_PRO_API_KEY" : "YOUR_API_KEY"
      }
   };
-  tokensArray = tokenId.split(",");
+  
   var response = UrlFetchApp.fetch(url,options);
   responseJSON = JSON.parse(response)
   
   var criptoPrices = []
   for (i=0; i < tokensArray.length;i++){
-    SpreadsheetApp.getActiveSpreadsheet().getSheetByName("YourSheetName").getRange(3,i+2).setValue(responseJSON.data[tokensArray[i]].quote.USD.price);
+    SpreadsheetApp.getActiveSpreadsheet().getSheetByName("CriptoValue").getRange(3,i+2).setValue(responseJSON.data[tokensArray[i]].quote.USD.price);
   }
+}
+function getTokens(){
+  var quantityOfTokens = parseInt(SpreadsheetApp.getActiveSpreadsheet().getSheetByName("CriptoValue").getRange(8,2).getValues());
+  tokensArray = []
+  for(i=2;i<=quantityOfTokens + 1;i++){
+    tokensArray.push(SpreadsheetApp.getActiveSpreadsheet().getSheetByName("CriptoValue").getRange(2,i).getValues());
+  }
+  return tokensArray;
 }
