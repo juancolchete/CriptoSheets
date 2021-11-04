8d518e184a6387fde24cd8fe65eb184cb6bc5fc6
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-11-04 20:28:05 -0300

    Integrate with pancake swap

--- getCriptoCurrenciesQuotation.js
+++ getCriptoCurrenciesQuotation.js
@@ -1,39 +1,69 @@
+var lastFilledColum = 0;
 function getCriptoCurrenciesQuotation() {
-  tokensArray = getTokensId();
-  var url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?'
-    + 'id='+tokensArray
-  var options = {
+  let quantityOfTokensCoinMarketCap = parseInt(activeSpreadsheet.getSheetByName("variables").getRange(3,2).getValues());
+  let tokensCoinMarketCap = getTokensId(2,quantityOfTokensCoinMarketCap);
+  let lastColumn = activeSpreadsheet.getSheetByName("CriptoValue").getLastColumn();
+  tokensCoinMarketCap = getTokensId(2,quantityOfTokensCoinMarketCap + 1);
+  tokensPancakeSwap = getTokensId(quantityOfTokensCoinMarketCap + 2,lastColumn);
+
+  getTokenQuotation(tokensCoinMarketCap,tokensPancakeSwap)
+
+  console.log()
+  
+}
+function getTokensId(start,end){
+  
+  tokensArray = [];
+  for(i=start;i<=end;i++){
+    tokensArray.push(activeSpreadsheet.getSheetByName("CriptoValue").getRange(2,i).getValues());
+  }
+  return tokensArray;
+}
+function getTokenInfo(){
+  let lastColumn = activeSpreadsheet.getSheetByName("CriptoValue").getLastColumn();
+  tokensArray = [];
+  for(j=2;j<=lastColumn;j++){
+    let criptoSymbol = activeSpreadsheet.getSheetByName("CriptoValue").getRange(1,j).getValues();
+    let id = activeSpreadsheet.getSheetByName("CriptoValue").getRange(2,j).getValues();
+    let quoutation = activeSpreadsheet.getSheetByName("CriptoValue").getRange(3,j).getValues();
+    let quantity =  activeSpreadsheet.getSheetByName("CriptoValue").getRange(4,j).getValues();
+    let value =  parseFloat(activeSpreadsheet.getSheetByName("CriptoValue").getRange(5,j).getValues());
+    tokensArray.push({id,criptoSymbol,quoutation,quantity,value});
+  }
+  return tokensArray;
+}
+function getTokenQuotation(tokensCoinMarketCap,tokensPancakeSwap){
+  getTokenQuotationCoinMarketCap(tokensCoinMarketCap);
+  getTokenQuotationPancakeSwap(tokensPancakeSwap);
+}
+function getTokenQuotationCoinMarketCap(tokensCoinMarketCap){
+  let url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?'
+    + 'id='+tokensCoinMarketCap
+  let options = {
      "method" : "get",
      "headers" : {
        "X-CMC_PRO_API_KEY" : "YOUR_API_KEY"
      }
   };
   
-  var response = UrlFetchApp.fetch(url,options);
+  let response = UrlFetchApp.fetch(url,options);
   responseJSON = JSON.parse(response);
   
-  for (i=0; i < tokensArray.length;i++){
-    SpreadsheetApp.getActiveSpreadsheet().getSheetByName("CriptoValue").getRange(3,i+2).setValue(responseJSON.data[tokensArray[i]].quote.USD.price);
+  for (i=0; i < tokensCoinMarketCap.length;i++){
+    activeSpreadsheet.getSheetByName("CriptoValue").getRange(3,i+2).setValue(responseJSON.data[tokensCoinMarketCap[i]].quote.USD.price);
+    lastFilledColum = i+2;
   }
 }
-function getTokensId(){
-  var quantityOfTokens = parseInt(SpreadsheetApp.getActiveSpreadsheet().getSheetByName("variables").getRange(3,2).getValues());
-  tokensArray = [];
-  for(i=2;i<=quantityOfTokens + 1;i++){
-    tokensArray.push(SpreadsheetApp.getActiveSpreadsheet().getSheetByName("CriptoValue").getRange(2,i).getValues());
+function getTokenQuotationPancakeSwap(tokensPancakeSwap){
+  let url = '';
+  let response = '';
+  let responseJSON= '';
+  for (i=0; i < tokensPancakeSwap.length;i++){
+    url = 'https://api.pancakeswap.info/api/v2/tokens/'+tokensPancakeSwap[i];
+    response = UrlFetchApp.fetch(url);
+    responseJSON = JSON.parse(response);
+    if(responseJSON.data.price != 0){
+      activeSpreadsheet.getSheetByName("CriptoValue").getRange(3,lastFilledColum+1).setValue(responseJSON.data.price);
+    }
   }
-  return tokensArray;
-}
-function getTokenInfo(){
-  var lastColumn = activeSpreadsheet.getSheetByName("CriptoValue").getLastColumn();
-  tokensArray = [];
-  for(j=2;j<=lastColumn;j++){
-    var criptoSymbol = activeSpreadsheet.getSheetByName("CriptoValue").getRange(1,j).getValues();
-    var id = activeSpreadsheet.getSheetByName("CriptoValue").getRange(2,j).getValues();
-    var quoutation = activeSpreadsheet.getSheetByName("CriptoValue").getRange(3,j).getValues();
-    var quantity =  activeSpreadsheet.getSheetByName("CriptoValue").getRange(4,j).getValues();
-    var value =  parseFloat(activeSpreadsheet.getSheetByName("CriptoValue").getRange(5,j).getValues());
-    tokensArray.push({id,criptoSymbol,quoutation,quantity,value});
-  }
-  return tokensArray;
 }
